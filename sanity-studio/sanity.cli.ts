97a7bb73ec1d0cde2309fe7cd63d9eb2c598c897
Josh Dale <joshuadale65@gmail.com>
2024-09-08 09:14:08 -0400

    structure

--- sanity-studio/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'ib1ciqv7',
-    dataset: 'production'
-  }
-})
--- sanity-studio/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import { HomePage } from "./pageSchemas/homePage";
-import { Page } from "./pageSchemas/page";
-
-export const schemaTypes = [
-    Page, HomePage
-]
