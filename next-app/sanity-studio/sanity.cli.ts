6272806d18fa684e48235f95cb8e3e331abbba33
Josh Dale <joshuadale65@gmail.com>
2024-09-08 09:12:26 -0400

    sanity studio moved

---
+++ next-app/sanity-studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ib1ciqv7',
+    dataset: 'production'
+  }
+})
---
+++ next-app/sanity-studio/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+import { deskStructure } from './deskStructure'
+
+const config = defineConfig({
+  name: 'default',
+  title: 'sanity-studio',
+
+  projectId: 'ib1ciqv7',
+  dataset: 'production',
+
+  basePath: "/studio",
+
+  plugins: [structureTool({ structure: deskStructure }), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
+export default config
